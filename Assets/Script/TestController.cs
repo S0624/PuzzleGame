ff8d5741305ff8b9ae9e6d32ed0cdae4c90ba2a5
S0624 <mmasyumaro.012345@gmail.com>
2023-10-12 18:53:07 +0900

    一個だけ置けるように

--- Assets/Script/TestController.cs
+++ Assets/Script/TestController.cs
@@ -7,21 +7,21 @@ using UnityEngine;
 public class TestController : MonoBehaviour
 {
     // ボードの横の最大値(6 * 14).
-    public const int BOARD_WIDTH = 6;
+    private const int _borad_Width = 6;
     // ボードの縦の最大値.
-    public const int BOARD_HEIGHT = 14;
+    private const int _borad_Height = 14;
 
     [SerializeField] GameObject _prefabCube = default!;
 
-    int[,] _board = new int[BOARD_HEIGHT, BOARD_WIDTH];
-    GameObject[,] _Cube = new GameObject[BOARD_HEIGHT, BOARD_WIDTH];
+    int[,] _board = new int[_borad_Height, _borad_Width];
+    GameObject[,] _Cube = new GameObject[_borad_Height, _borad_Width];
 
     // ボードの中を全消しする(クリアする).
     public void ClearAll()
     {
-        for (int y = 0; y < BOARD_HEIGHT; y++)
+        for (int y = 0; y < _borad_Height; y++)
         {
-            for (int x = 0; x < BOARD_WIDTH; x++)
+            for (int x = 0; x < _borad_Width; x++)
             {
                 _board[y, x] = 0;
 
@@ -42,33 +42,34 @@ public class TestController : MonoBehaviour
         // 生成する前に消す.
         ClearAll();
 
-        for (int y = 0; y < BOARD_HEIGHT; y++)
+        for (int y = 0; y < _borad_Height; y++)
         {
-            for (int x = 0; x < BOARD_WIDTH; x++)
+            for (int x = 0; x < _borad_Width; x++)
             {
-                Settle(new Vector2Int(x, y), Random.Range(0, (int)ColorType.max));
+                IsSetCube(new Vector2Int(x, y), Random.Range(0, (int)ColorType.max));
             }
         }
     }
+    // 範囲外じゃないかの判定
     public static bool IsValidated(Vector2Int pos)
     {
-        return 0 <= pos.x && pos.x < BOARD_WIDTH
-            && 0 <= pos.y && pos.y < BOARD_HEIGHT;
+        return 0 <= pos.x && pos.x < _borad_Width
+            && 0 <= pos.y && pos.y < _borad_Height;
     }
-
-    public bool CanSettle(Vector2Int pos)
+    // フィールド内におけるかどうかの判定
+    public bool IsCanSetCube(Vector2Int pos)
     {
         if (!IsValidated(pos)) return false;
 
         return 0 == _board[pos.y, pos.x];
     }
-
-    public bool Settle(Vector2Int pos, int val)
+    // フィールド内にセットする
+    public bool IsSetCube(Vector2Int pos, int val)
     {
-        if (!CanSettle(pos)) return false;
+        if (!IsCanSetCube(pos)) return false;
 
         _board[pos.y, pos.x] = val;
-
+        // もし中身が入っていたらエラー表記を出す
         Debug.Assert(_Cube[pos.y, pos.x] == null);
         Vector3 world_position = transform.position + new Vector3(pos.x, pos.y, 0.0f);
         _Cube[pos.y, pos.x] = Instantiate(_prefabCube, world_position, Quaternion.identity, transform);
@@ -76,7 +77,20 @@ public class TestController : MonoBehaviour
 
         return true;
     }
-
+    // Y軸(下方向)におけるかどうかのチェック
+    public bool IsNextCube(Vector2Int pos)
+    {
+        // ゼロだったらおけるようにする
+        if(pos.y == 0)
+        {
+            return true;
+        }
+        if(_Cube[pos.y - 1, pos.x] != null)
+        {
+            return true;
+        }
+        return false;
+    }
 
     //// Start is called before the first frame update
     //void Start()
--- Assets/Script/TestMove.cs
+++ Assets/Script/TestMove.cs
@@ -5,12 +5,18 @@ using UnityEngine.InputSystem;
 
 public class TestMove : MonoBehaviour
 {
+    private InputAction _action;
     private TestInputManager _testInput;
     private Rigidbody rb;
     private Vector3 force;
     // 移動速度.
     private float _speed;
-    float currentSpeed;
+    private float currentSpeed;
+
+    private GameObject _fieldObject;
+    private Vector2Int _cubePos = new Vector2Int(0,14);
+    private Vector3 _cubePostemp;
+    private int _timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +26,12 @@ public class TestMove : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         force = new Vector3(1.0f, 0.0f, 0.0f);
         _speed = 5.0f;
+
+        this.transform.position = transform.position + new Vector3(_cubePos.x, _cubePos.y, 0);
+        _cubePostemp = this.transform.position;
+        _fieldObject = GameObject.Find("Board");
+        //_testField.GetComponent<TestController>().IsSetCube(_testPos,0);
+
     }
 
     // Update is called once per frame
@@ -29,53 +41,99 @@ public class TestMove : MonoBehaviour
     }
     void FixedUpdate()
     {
+        _timer++;
+        //this.transform.position = transform.position + new Vector3(_testPos.x, _testPos.y, 0);
+        //Debug.Log(_testPos);
         // 方向キーの入力取得
-        Vector2 stickVector2 = this._testInput.Piece.Move.ReadValue<Vector2>();
+        Vector2 moveInput = this._testInput.Piece.Move.ReadValue<Vector2>();
         // 上下左右に動かす
         //transform.Translate(stickVector2 * _speed * Time.deltaTime);
         //Vector2 a = stickVector2 * _speed * Time.deltaTime;
         // HACK とりあえず手を動かす用の処理(後でもっといい方法に書き直したいな).
-        if (stickVector2.y < 0)
+        _action = _testInput.Piece.Move;
+        if (moveInput.y < 0 && _action.WasPressedThisFrame())
         {
-            Debug.Log("動いて");
+            if (!_fieldObject.GetComponent<TestController>().IsNextCube(_cubePos))
+            //if (_cubePos.y > 0)
+            {
+                _cubePos.y--;
+                _timer = 0;
+                CubePos(0, -1);
+            }
+            else
+            {
+                _timer = (int)(60 * 1.2f);
+            }
+            //_testField.GetComponent<TestController>().ClearAll();
+            //_testField.GetComponent<TestController>().IsSetCube(_testPos,0);
             // 下.
-            MoveObject2(-1);
-            MoveObject(new Vector3(0.0f, currentSpeed, 0.0f));
+            //MoveObject2(-1);
+            //MoveObject(new Vector3(0.0f, currentSpeed, 0.0f));
         }
-        if (stickVector2.x > 0)
+        else if (moveInput.x > 0 && _action.WasPressedThisFrame())
         {
+            _cubePos.x++;
+            CubePos(1, 0);
+            //_testField.GetComponent<TestController>().ClearAll();
+            //_testField.GetComponent<TestController>().IsSetCube(_testPos,0);
             // 右.
-            this.transform.position += new Vector3(5.0f, 0.0f, 0.0f);
             //MoveObject2(10);
             //MoveObject(new Vector3(currentSpeed, 0.0f, 0.0f));
         }
-        if (stickVector2.x < 0)
+        else if (moveInput.x < 0 && _action.WasPressedThisFrame())
         {
+            _cubePos.x--;
+            CubePos(-1, 0);
+            //_testField.GetComponent<TestController>().ClearAll();
+            //_testField.GetComponent<TestController>().IsSetCube(_testPos,0);
             // 左.
-            MoveObject2(-10);
-            MoveObject(new Vector3(currentSpeed, 0.0f, 0.0f));
+            //MoveObject2(-10);
+            //MoveObject(new Vector3(currentSpeed, 0.0f, 0.0f));
         }
-
-
-        //// HACK 時間での落下処理(仮)
-        //MoveObject2(-1);
-        //MoveObject(new Vector3(0.0f, currentSpeed, 0.0f));
-
-    }
-
-    void MoveObject2(float test = 1)
-    {
-        //指定したスピードから現在の速度を引いて加速力を求める
-        currentSpeed = (_speed - rb.velocity.magnitude) * test;
-    }
-    void MoveObject(Vector3 currentspeed)
-    {
-        if (rb.velocity.magnitude < 10)
+        else
         {
-            //調整された加速力で力を加える
-            rb.AddForce(currentspeed);
+            //MoveObject2(0);
+            //MoveObject(new Vector3(0.0f, 0.0f, 0.0f));
         }
-    }
-    
+        // 下にキューブがあったら進まないようにしたい
+        if(_timer > 60 * 1.2 && _fieldObject.GetComponent<TestController>().IsNextCube(_cubePos))
+        {
+            _fieldObject.GetComponent<TestController>().IsSetCube(_cubePos, 0);
+            _cubePos = new Vector2Int(0, 14);
+            this.transform.position = _cubePostemp;
+        }
 
+        // HACK 時間での落下処理(仮)
+        if(_timer > 60 * 1.2)
+        {
+            _cubePos.y--;
+            CubePos(0, -1);
+            _timer = 0;
+        }
+
+    }
+
+    //void MoveObject2(float test = 1)
+    //{
+    //    //指定したスピードから現在の速度を引いて加速力を求める
+    //    currentSpeed = (_speed - rb.velocity.magnitude) * test;
+    //}
+    //void MoveObject(Vector3 currentspeed)
+    //{
+    //    if (rb.velocity.magnitude < 10)
+    //    {
+    //        Debug.Log(rb.velocity.magnitude);
+    //        //調整された加速力で力を加える
+    //        rb.AddForce(currentspeed);
+    //    }
+    //    else
+    //    {
+    //        rb.AddForce(Vector3.zero);
+    //    }
+    //}
+
+    private void CubePos(float x = 0, float y = 0)
+    {
+        this.transform.position = transform.position + new Vector3(x, y, 0);
+    }
 }
